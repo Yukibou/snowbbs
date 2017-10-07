7f09b80b34ba26a725b70aa614ed30e91fadef54
yukiya <yukiya@sonicgarden.jp>
2017-10-07 14:53:58 +0900

    create area

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007022736) do
+ActiveRecord::Schema.define(version: 20171007053703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "areas", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "observations", force: :cascade do |t|
     t.integer  "user_id"
