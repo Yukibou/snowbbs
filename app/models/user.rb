ea9c83a387dbca43f0621d3dddd740e74b0f85d4
yukiya <yukiya@sonicgarden.jp>
2017-10-10 08:02:30 +0900

    admin ログイン調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :observations
-  enum role: [:user, :vip, :admin]
+  enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
