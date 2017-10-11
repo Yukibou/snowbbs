05d00ca3434e93ceec9b517d9b262124601d34de
yukiya <yukiya@sonicgarden.jp>
2017-10-11 10:45:13 +0900

    ユーザーモデルに名前を追加してサインアップできるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
 end
