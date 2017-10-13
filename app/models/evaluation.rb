5bf3a3a6e23d8e5ea6f423e4957b35739f3de59f
yukiya <yukiya@sonicgarden.jp>
2017-10-13 16:26:17 +0900

    セレクトボックス情報を追加

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,3 +1,18 @@
 class Evaluation < ApplicationRecord
+  extend Enumerize
   belongs_to :observation, inverse_of: :evaluations
+
+  enumerize :problem, in: [
+      :dry_loose_snow,
+      :storm_slab,
+      :wind_slab,
+      :persistent_slab,
+      :deep_persistent_slab,
+      :glide_slab,
+      :wet_loose_snow,
+      :wet_slab
+  ]
+  enumerize :triggering, in: [:certain, :very_likely, :likely, :possible, :unlikely]
+  enumerize :sensitivity, in: [:unreactive, :stubborn, :reactive, :touchy]
+  enumerize :spatial, in: [:isolated, :specific, :widespread]
 end
