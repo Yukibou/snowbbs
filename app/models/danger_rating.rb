5a4effa8101ae4ca2e87e660d7a10473d36b13d5
yukiya <yukiya@sonicgarden.jp>
2017-11-23 19:44:33 +0900

    No Rating追加

--- app/models/danger_rating.rb
+++ app/models/danger_rating.rb
@@ -2,9 +2,9 @@ class DangerRating < ApplicationRecord
   extend Enumerize
   belongs_to :observation, inverse_of: :danger_rating
 
-  enumerize :alpine_zone, in: [:extreme, :high, :considerable, :moderate, :low]
-  enumerize :treeline_zone, in: [:extreme, :high, :considerable, :moderate, :low]
-  enumerize :below_treeline_zone, in: [:extreme, :high, :considerable, :moderate, :low]
+  enumerize :alpine_zone, in: [:extreme, :high, :considerable, :moderate, :low, :no_rating]
+  enumerize :treeline_zone, in: [:extreme, :high, :considerable, :moderate, :low, :no_rating]
+  enumerize :below_treeline_zone, in: [:extreme, :high, :considerable, :moderate, :low, :no_rating]
   enumerize :alpine_confidence, in: [:good, :fair, :poor]
   enumerize :treeline_confidence, in: [:good, :fair, :poor]
   enumerize :below_treeline_confidence, in: [:good, :fair, :poor]
