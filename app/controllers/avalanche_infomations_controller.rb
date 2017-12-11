c59af75f328ac2b941e79306ceec65e4e6f4865f
yukiya <yukiya@sonicgarden.jp>
2017-12-11 15:43:03 +0900

    ユーザー向けの不要なアクションを削除

--- app/controllers/avalanche_infomations_controller.rb
+++ app/controllers/avalanche_infomations_controller.rb
@@ -1,6 +1,6 @@
 class AvalancheInfomationsController < ApplicationController
-  before_action :set_avalanche_infomation, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_avalanche_infomation, only: [:index, :new, :create]
+  before_action :set_avalanche_infomation, only: [:show]
+  before_action :authorize_avalanche_infomation, only: [:index]
   after_action :verify_authorized
 
   def index
@@ -9,48 +9,6 @@ class AvalancheInfomationsController < ApplicationController
 
   def show
   end
-
-  # def new
-  #   @avalanche_infomation = current_user.avalanche_infomations.new
-  #   @avalanche_infomation.avalanche_problems.build
-  # end
-  #
-  # def edit
-  # end
-  #
-  # def create
-  #   @avalanche_infomation = current_user.avalanche_infomations.new(avalanche_infomation_params)
-  #
-  #   respond_to do |format|
-  #     if @avalanche_infomation.save
-  #       format.html {redirect_to @avalanche_infomation, notice: 'Avalanche infomation was successfully created.'}
-  #       format.json {render :show, status: :created, location: @avalanche_infomation}
-  #     else
-  #       format.html {render :new}
-  #       format.json {render json: @avalanche_infomation.errors, status: :unprocessable_entity}
-  #     end
-  #   end
-  # end
-  #
-  # def update
-  #   respond_to do |format|
-  #     if @avalanche_infomation.update(avalanche_infomation_params)
-  #       format.html {redirect_to @avalanche_infomation, notice: 'Avalanche infomation was successfully updated.'}
-  #       format.json {render :show, status: :ok, location: @avalanche_infomation}
-  #     else
-  #       format.html {render :edit}
-  #       format.json {render json: @avalanche_infomation.errors, status: :unprocessable_entity}
-  #     end
-  #   end
-  # end
-  #
-  # def destroy
-  #   @avalanche_infomation.destroy
-  #   respond_to do |format|
-  #     format.html {redirect_to avalanche_infomations_url, notice: 'Avalanche infomation was successfully destroyed.'}
-  #     format.json {head :no_content}
-  #   end
-  # end
 
   private
   def set_avalanche_infomation
@@ -61,28 +19,4 @@ class AvalancheInfomationsController < ApplicationController
   def authorize_avalanche_infomation
     authorize policy_scope(AvalancheInfomation)
   end
-
-  # def avalanche_infomation_params
-  #   params.require(:avalanche_infomation).permit(
-  #       :announced_at,
-  #       :area_id,
-  #       :alpine_zone,
-  #       :alpine_trend,
-  #       :alpine_comments,
-  #       :alpine_confidence,
-  #       :treeline_zone,
-  #       :treeline_trend,
-  #       :treeline_comments,
-  #       :treeline_confidence,
-  #       :below_treeline_zone,
-  #       :below_treeline_trend,
-  #       :below_treeline_comments,
-  #       :below_treeline_confidence,
-  #       :avalanche,
-  #       :snowpack,
-  #       :weather,
-  #       :travel_advisory,
-  #       avalanche_problems_attributes: AvalancheProblem::REGISTRABLE_ATTRIBUTES
-  #       )
-  # end
 end
--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,6 +1,6 @@
 class ObservationsController < ApplicationController
-  before_action :set_observation, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_observation, only: [:index, :new, :create]
+  before_action :set_observation, only: [:show]
+  before_action :authorize_observation, only: [:index]
   after_action :verify_authorized
 
   def index
@@ -14,49 +14,6 @@ class ObservationsController < ApplicationController
   def show
   end
 
-  # def new
-  #   @observation = current_user.observations.new
-  #   @observation.evaluations.build
-  #   @observation.build_danger_rating
-  # end
-  #
-  # def edit
-  # end
-  #
-  # def create
-  #   @observation = current_user.observations.new(observation_params)
-  #
-  #   respond_to do |format|
-  #     if @observation.save
-  #       format.html {redirect_to @observation, notice: 'Observation was successfully created.'}
-  #       format.json {render :show, status: :created, location: @observation}
-  #     else
-  #       format.html {render :new}
-  #       format.json {render json: @observation.errors, status: :unprocessable_entity}
-  #     end
-  #   end
-  # end
-  #
-  # def update
-  #   respond_to do |format|
-  #     if @observation.update(observation_params)
-  #       format.html {redirect_to @observation, notice: 'Observation was successfully updated.'}
-  #       format.json {render :show, status: :ok, location: @observation}
-  #     else
-  #       format.html {render :edit}
-  #       format.json {render json: @observation.errors, status: :unprocessable_entity}
-  #     end
-  #   end
-  # end
-  #
-  # def destroy
-  #   @observation.destroy
-  #   respond_to do |format|
-  #     format.html {redirect_to observations_url, notice: 'Observation was successfully destroyed.'}
-  #     format.json {head :no_content}
-  #   end
-  # end
-
   private
   def set_observation
     @observation = Observation.find(params[:id])
@@ -66,41 +23,4 @@ class ObservationsController < ApplicationController
   def authorize_observation
     authorize policy_scope(Observation)
   end
-
-  # def observation_params
-  #   params.require(:observation).permit(
-  #       :observation_at,
-  #       :area_id,
-  #       :avalanche_infomation_id,
-  #       :mountain,
-  #       :location,
-  #       :co_worker,
-  #       :elevation_low,
-  #       :elevation_high,
-  #       :temperature_low,
-  #       :temperature_low_hrs,
-  #       :temperature_low_elevation,
-  #       :temperature_high,
-  #       :temperature_high_hrs,
-  #       :temperature_high_elevation,
-  #       :sky,
-  #       :sky_comments,
-  #       :wind_speed,
-  #       :wind_direction,
-  #       :wind_comments,
-  #       :precipitation_type,
-  #       :precipitation_rate,
-  #       :precipitation_comments,
-  #       :avalanches,
-  #       :avalanche_occurred,
-  #       :snowpack,
-  #       :spin_nos,
-  #       :comments,
-  #       :publish,
-  #       :movie_url,
-  #       {images: []},
-  #       :general_comments,
-  #       evaluations_attributes: Evaluation::REGISTRABLE_ATTRIBUTES,
-  #       danger_rating_attributes: DangerRating::REGISTRABLE_ATTRIBUTES)
-  # end
 end
