4caaae89ac71d2617508837aa9bd82fb28d96419
yukiya <yukiya@sonicgarden.jp>
2017-12-15 16:56:31 +0900

    iframe許容

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,12 @@ module Snowbbs
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    # iframe許容
+    config.action_dispatch.default_headers["X-Frame-Options"] = "DENY"
   end
 end
