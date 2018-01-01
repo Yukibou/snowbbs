dcc2266ca2d91e2ab89558aad6b0cbe3d72eda66
yukiya <yukiya@sonicgarden.jp>
2018-01-02 00:11:01 +0900

    信頼度の区分の変更

--- app/models/danger_rating.rb
+++ app/models/danger_rating.rb
@@ -5,9 +5,19 @@ class DangerRating < ApplicationRecord
   enumerize :alpine_zone, in: [:extreme, :high, :considerable, :moderate, :low, :no_rating]
   enumerize :treeline_zone, in: [:extreme, :high, :considerable, :moderate, :low, :no_rating]
   enumerize :below_treeline_zone, in: [:extreme, :high, :considerable, :moderate, :low, :no_rating]
-  enumerize :alpine_confidence, in: [:good, :fair, :low]
-  enumerize :treeline_confidence, in: [:good, :fair, :low]
-  enumerize :below_treeline_confidence, in: [:good, :fair, :low]
+  enumerize :alpine_confidence, in: [:good, :fair, :high, :moderate, :low]
+  enumerize :treeline_confidence, in: [:good, :fair, :high, :moderate, :low]
+  enumerize :below_treeline_confidence, in: [:good, :fair, :high, :moderate, :low]
 
   REGISTRABLE_ATTRIBUTES = %i(id alpine_zone treeline_zone below_treeline_zone alpine_comments treeline_comments below_treeline_comments alpine_confidence treeline_confidence below_treeline_confidence)
+
+  def convert_confidence
+    self.alpine_confidence = :high if self.alpine_confidence == :good
+    self.alpine_confidence = :moderate if self.alpine_confidence == :fair
+    self.treeline_confidence = :high if self.treeline_confidence == :good
+    self.treeline_confidence = :moderate if self.treeline_confidence == :fair
+    self.below_treeline_confidence = :high if self.below_treeline_confidence == :good
+    self.below_treeline_confidence = :moderate if self.below_treeline_confidence == :fair
+    save!
+  end
 end
