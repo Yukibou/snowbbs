19bba6e8e019db51989c29f6df7d6812b81d81b3
yukiya <yukiya@sonicgarden.jp>
2018-05-27 16:51:34 +0900

    上位２０名をはずす

--- app/controllers/members/dashboards_controller.rb
+++ app/controllers/members/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class Members::DashboardsController < Members::ApplicationController
   def index
-    @users = User.joins(:observations).group('users.id').order('count(users.id) DESC').limit 20
+    @users = User.joins(:observations).group('users.id').order('count(users.id) DESC')
     @areas = Area.joins(:observations).group('areas.id').order('count(areas.id) DESC')
     @observation_counts_by_date = Observation.group("DATE(created_at)").order("DATE(created_at)").count
   end
