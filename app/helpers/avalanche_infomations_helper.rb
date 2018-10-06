effdf0a514873c46a5e4cec500a658fa7d39c34b
yukiya <yukiya@sonicgarden.jp>
2018-10-07 00:32:52 +0900

    標高帯表示を新しいバージョンにした

--- app/helpers/avalanche_infomations_helper.rb
+++ app/helpers/avalanche_infomations_helper.rb
@@ -1,6 +1,2 @@
 module AvalancheInfomationsHelper
-  def zone_image(zone, rating, trend)
-    trend = '0' if rating == :no_rating
-    image_tag "#{zone}-#{rating.to_s}-#{trend.to_s}.jpg"
-  end
 end
