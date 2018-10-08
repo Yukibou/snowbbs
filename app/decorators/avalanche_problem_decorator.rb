8b93b2e830eca436ac4f75c7c5fc2a96ac0590d9
yukiya <yukiya@sonicgarden.jp>
2018-10-08 18:00:59 +0900

    雪崩情報、留意すべき雪崩を追加できるようにした

---
+++ app/decorators/avalanche_problem_decorator.rb
@@ -0,0 +1,21 @@
+class AvalancheProblemDecorator
+  def initialize(avalanche_problem)
+    @avalanche_problem = avalanche_problem
+    @avalanche_problem.zone_details.build(zone: :alpine)
+    @avalanche_problem.zone_details.build(zone: :treeline)
+    @avalanche_problem.zone_details.build(zone: :below_treeline)
+    @avalanche_problem
+  end
+
+  def formatted_created_at
+    @avalanche_problem.created_at.to_formatted_s(:short)
+  end
+
+  def method_missing(method_sym, *args)
+    if @avalanche_problem.respond_to?(method_sym)
+      @avalanche_problem.send(method_sym, *args)
+    else
+      super
+    end
+  end
+end
