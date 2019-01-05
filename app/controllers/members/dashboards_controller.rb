18693cf832dcc58248d7044aa3335d31b763d77e
yukiya <yukiya.ueda@gmail.com>
2019-01-05 21:21:25 +0900

    ダッシュボードの並び変更

--- app/controllers/members/dashboards_controller.rb
+++ app/controllers/members/dashboards_controller.rb
@@ -2,6 +2,6 @@ class Members::DashboardsController < Members::ApplicationController
   def index
     @users = User.joins(:observations).group('users.id').order('count(users.id) DESC')
     @areas = Area.joins(:observations).group('areas.id').order('count(areas.id) DESC')
-    @observation_counts_by_date = Observation.group("DATE(created_at)").order("DATE(created_at)").count
+    @observation_counts_by_date = Observation.group("DATE(created_at)").order("DATE(created_at) DESC").count
   end
 end
